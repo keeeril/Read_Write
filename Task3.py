c6f48b24249013c09147d313d07d6cafd5a0fc85
Kirill <keeeril@bk.ru>
2021-11-11 23:37:52 +0300

    switched to "w"

--- Task3.py
+++ Task3.py
@@ -8,7 +8,7 @@ def do_new_file():
             count_line = len(file.readlines())
         with open(path, 'rt', encoding='utf-8') as file:
             data[path] = [count_line, file.read().strip()]
-    with open('new.txt', 'a', encoding='utf-8') as new_file:
+    with open('new.txt', 'w', encoding='utf-8') as new_file:
         data = sorted(data.items(), key=lambda x: x[1])
         for i in data:
             new_file.write(i[0] + '\n')
